d4eac3d6d10c1b559e379b510e9b74ca0598d6a7
Joshua Corbett <jcorbett@therocketeers.space>
2021-09-14 17:07:20 -0500

    move comments again

--- azure-windows/versions.tf
+++ azure-windows/versions.tf
@@ -14,7 +14,7 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-
+#This references the Terraform Cloud workspace and organization. 
   backend "remote" {
     organization = "rocketlab"
 
@@ -23,4 +23,3 @@ terraform {
     }
   }
 }
-#This references the Terraform Cloud workspace and organization. 
